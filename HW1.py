b51d78c7440bab182b57668316b846296095afd8
Ari Siesser <asiesser3@gatech.edu>
2017-02-13 12:43:46 -0500

    Now keeps track of how much time planes spent circling each airport. Also modified the code to move the timeouts to the airport methods and out of the plane fly loop.

--- HW1.py
+++ HW1.py
@@ -1,7 +1,4 @@
 """
-Practice putting this code on GIT
-
-
 Scenario:
   We have 5 airports. The airports are each a certain distance away from each other.
   Planes should travel between the different airports. Planes should have attributes such as speed and capacity.
@@ -36,22 +33,52 @@ class Airport:
         # initialize some attributes to keep track of metrics at the airport
         self.num_passengers_arrived = 0
         self.num_passengers_departed = 0
+        self.time_circling = 0
 
     def plane_arrives(self, plane):
-        """The arrival process takes a ```plane``` and tries to schedule it for landing
+        """A ```plane``` arrives at the airport and requests runway to land. We must update the arrival time
+        attribute of the plane in order to keep track of how long the plane spent circling before it landed.
+
 
         """
-        pass
+        print('Time {}: {} arrives at airport {}'.format(self.env.now, plane.name, self.name))
+        plane.arrival_time = self.env.now
+        # request the runway
+        with self.runway.request() as request:
+            # wait for it to become available
+            yield request
+            print('Time {}: {} begins landing at airport {}'.format(self.env.now, plane.name, self.name))
 
-    def plane_lands(self, plane):
-        """The landing process takes a ```plane```  and XXXXXX"""
-        yield self.env.timeout(self.required_runway_time)
+            # update the time_circling attribute of the airport
+            self.time_circling += self.env.now - plane.arrival_time
+
+            # keep runway occupied while plane is landing
+            yield self.env.timeout(self.required_runway_time)
+            print('Time {}: {} completes landing at airport {}. It is now being prepared for its next flight'.format(
+                self.env.now, plane.name, self.name))
+
+        # now prepare the plane for it's next flight
+        yield self.env.process(self.prepare_plane_for_next_flight(plane))
 
     def prepare_plane_for_next_flight(self, plane):
+        """Here the ```plane``` incurs a delay while it is being prepared for the next flight.
+
+        """
         yield self.env.timeout(self.required_time_on_ground)
+        print('Time {}: {} is now ready for its next flight'.format(self.env.now, plane.name))
 
     def plane_departs(self, plane):
-        yield self.env.timeout(self.required_runway_time)
+        with self.runway.request() as request:
+            # request the runway to take off
+            yield request
+
+            # take off when the runway is available - occupy the resource for required_runway_time
+            print('Time {}: {} using runway to depart {}'.format(self.env.now, plane.name, self.name))
+            yield self.env.timeout(self.required_runway_time)
+
+            # the plane has finished using the runway to depart and is now in the air
+            print('Time {}: {} has left {} en route to {} carrying {} passengers'.format(
+                self.env.now, plane.name, self.name, plane.destination.name, plane.num_passengers))
 
 
 class Plane:
@@ -67,27 +94,17 @@ class Plane:
         self.capacity = capacity
         self.num_passengers = num_passengers
         self.speed = speed
+        self.arrival_time = None
         self.in_the_air = False
+
 
     def fly(self):
         """The fly process causes a plane to depart from one airport to another airport and request a runway to land.
 
         """
         while True:
-            print('%s arrives at airport %s at time %.2f.' % (self.name, self.destination.name, self.env.now))
-            with self.destination.runway.request() as request:
-                # wait for access
-                yield request
-
-                # land
-                print('%s clear to land at airport %s at time %.2f.' % (self.name, self.destination.name, self.env.now))
-                yield self.env.process(self.destination.plane_lands(self.name))
-
-                print('%s is done with runway at airport %s at time %.2f.' % (self.name, self.destination.name, self.env.now))
-
-            # the plane is unable to fly again until ```required_time_on_ground``` has elapsed
-            yield self.env.process(self.destination.prepare_plane_for_next_flight(self.name))
-            #print('%s is now clear to depart airport %s at time %.2f.' % (self.name, self.destination.name, self.env.now))
+            # the plane must arrive at the airport and land
+            yield self.env.process(self.destination.plane_arrives(self))
 
             # choose a new airport and calculate flight time
             current_airport = self.destination
@@ -95,22 +112,14 @@ class Plane:
                 [self.airports[i] for i in range(len(self.airports)) if self.airports[i] != self.destination])
             self.destination = new_dest
             flightDist = self.distances[current_airport][new_dest]
-            flightTime = flightDist / self.speed
+            flightTime = int((flightDist / self.speed)  * 60)
             self.num_passengers = random.randint(0,200)
 
-            # request the runway to take off
-            with current_airport.runway.request() as request:
-                # wait for access
-                yield request
-
-                # take off
-                print('{} using runway to depart {} at time {}.'.format(
-                    self.name, current_airport.name, self.env.now))
-                yield self.env.process(current_airport.plane_departs(self.name))
+            # request the runway and depart to new destination
+            yield self.env.process(current_airport.plane_departs(self))
 
             # fly to new destination
-            print('{} has left {} en route to {} carrying {} passengers at time {}. Flight time is {} minutes'.format(
-                self.name, current_airport.name, new_dest.name, str(self.num_passengers), self.env.now, str(flightTime)))
+            print('flight time for {} is {} minutes'.format(self.name, flightTime))
             yield self.env.timeout(flightTime)
 
 
@@ -118,8 +127,8 @@ def main():
     """This is the main execution of the simulation. Here we will create instances of airports and planes.
 
     """
-    num_airports = 5
-    num_planes = 4
+    num_airports = 2
+    num_planes = 5
 
     env = simpy.Environment()
 
@@ -136,12 +145,16 @@ def main():
     # create some planes
     planes = []
     for i in range(num_planes):
-        plane = Plane(env, 'plane ' + str(i), airports, distances, airports[0], 200, random.randint(0,200), 550/60)
+        plane = Plane(env, 'plane ' + str(i), airports, distances, airports[0], 200, random.randint(0,200), 550)
         planes.append(plane)
         env.process(plane.fly())
 
     # run the simulation
-    env.run(until=100)
+    env.run(until=1000)
+
+    # look at how much time planes spent circling the different airports
+    for airport in airports:
+        print('total circling time at airport {} was {} minutes'.format(airport.name, airport.time_circling))
 
 
 if __name__ == '__main__':

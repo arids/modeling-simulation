0189ac0b761430629008efa3a27de44175ffaf5a
Ari Siesser <asiesser3@gatech.edu>
2017-02-13 23:57:52 -0500

    final submission

--- HW1.py
+++ HW1.py
@@ -11,9 +11,13 @@ Scenario:
 """
 
 import simpy
-import random
+import sys
 import numpy as np
 import pandas as pd
+import argparse
+
+# send standard out to a file
+#sys.stdout = open('Simulation_log.txt', 'w')
 
 
 class Airport:
@@ -21,14 +25,12 @@ class Airport:
     only accommodate one plane at a time.
 
     """
-    def __init__(self, env, name, num_runways, required_runway_time, required_time_on_ground, lat, long):
+    def __init__(self, env, name, num_runways, required_runway_time, required_time_on_ground):
         self.env = env
         self.name = name
         self.runway = simpy.Resource(env, num_runways)
         self.required_runway_time = required_runway_time
         self.required_time_on_ground = required_time_on_ground
-        self.lat = lat
-        self.long = long
 
         # initialize some attributes to keep track of metrics at the airport
         self.num_passengers_arrived = 0
@@ -51,6 +53,7 @@ class Airport:
 
             # update the time_circling attribute of the airport
             self.time_circling += self.env.now - plane.arrival_time
+            circling_time.loc[(var_num_planes, self.name), 'circling_time'] = self.time_circling
 
             # keep runway occupied while plane is landing
             yield self.env.timeout(self.required_runway_time)
@@ -95,8 +98,6 @@ class Plane:
         self.num_passengers = num_passengers
         self.speed = speed
         self.arrival_time = None
-        self.in_the_air = False
-
 
     def fly(self):
         """The fly process causes a plane to depart from one airport to another airport and request a runway to land.
@@ -108,12 +109,12 @@ class Plane:
 
             # choose a new airport and calculate flight time
             current_airport = self.destination
-            new_dest = random.choice(
+            new_dest = np.random.choice(
                 [self.airports[i] for i in range(len(self.airports)) if self.airports[i] != self.destination])
             self.destination = new_dest
             flightDist = self.distances[current_airport][new_dest]
             flightTime = int((flightDist / self.speed)  * 60)
-            self.num_passengers = random.randint(0,200)
+            self.num_passengers = np.random.randint(0,200)
 
             # request the runway and depart to new destination
             yield self.env.process(current_airport.plane_departs(self))
@@ -127,34 +128,70 @@ def main():
     """This is the main execution of the simulation. Here we will create instances of airports and planes.
 
     """
-    num_airports = 2
-    num_planes = 5
+    # take in the command line arguments
+    parser = argparse.ArgumentParser(description='Run an airport simulation')
+    parser.add_argument("-planes", type=int, help="number of airplanes", required=False, default=5)
+    parser.add_argument("-airports", type=int, help="number of airports", required=False, default=5)
+    parser.add_argument("-runways", type=int, help="number of runways at each airport", required=False, default=1)
+    parser.add_argument("-run_multiple", type=bool, help="run multiple iterations for different numbers of planes",
+                        required=False, default=False)
+    parser.add_argument("-simulation_time", type=int, help="number of minutes the simulation should run for",
+                        required=False, default=1000)
 
-    env = simpy.Environment()
+    args = parser.parse_args()
 
-    # create some airports
-    airports = []
-    for i in range(num_airports):
-        airports.append(Airport(env, 'A' + str(i), 1, 10, 15, None, None))
+    num_airports = args.airports
+    num_planes = args.planes
 
-    # create a random distnace matrix for the airport
-    distances = np.random.randint(600, 3000, size=(num_airports, num_airports))
-    distances = np.triu(distances, 1) + np.triu(distances,1).T
-    distances = pd.DataFrame(distances, columns = airports, index = airports)
+    #num_airports = 5
+    #num_planes = 50
+    #n_runways = 2
 
-    # create some planes
-    planes = []
-    for i in range(num_planes):
-        plane = Plane(env, 'plane ' + str(i), airports, distances, airports[0], 200, random.randint(0,200), 550)
-        planes.append(plane)
-        env.process(plane.fly())
+    # initialize a dataframe to keep track of the time circling at each airport
+    ind = pd.MultiIndex.from_product([np.array(list(range(1,num_planes+1))),
+                                        np.array(['A0', 'A1', 'A2', 'A3', 'A4'])])
+    global circling_time
+    circling_time = pd.DataFrame(columns=['circling_time'], index=ind, data=np.zeros(len(ind)))
 
-    # run the simulation
-    env.run(until=1000)
+    # determine how many simulation runs based off the -run_multiple arg
+    if args.run_multiple:
+        begin = 1
+    else:
+        begin = num_planes
 
-    # look at how much time planes spent circling the different airports
-    for airport in airports:
-        print('total circling time at airport {} was {} minutes'.format(airport.name, airport.time_circling))
+
+    global var_num_planes
+    for var_num_planes in range(begin,num_planes+1):
+        num_planes = var_num_planes
+
+        # set the random seed in order to make the simulation repeatable
+        np.random.seed(0)
+
+        # generate the simpy environment
+        env = simpy.Environment()
+
+        # create some airports
+        airports = []
+        for i in range(num_airports):
+            airport = Airport(env, name='A' + str(i), num_runways=args.runways, required_runway_time=10,
+                                    required_time_on_ground=15)
+            airports.append(airport)
+
+        # create a random distnace matrix for the airport
+        distances = np.random.randint(600, 3000, size=(num_airports, num_airports))
+        distances = np.triu(distances, 1) + np.triu(distances,1).T
+        distances = pd.DataFrame(distances, columns = airports, index = airports)
+
+        # create some planes
+        planes = []
+        for i in range(num_planes):
+            plane = Plane(env, 'plane ' + str(i), airports, distances, np.random.choice(airports), 200,
+                          np.random.randint(0,200), 550)
+            planes.append(plane)
+            env.process(plane.fly())
+
+        # run the simulation
+        env.run(until=args.simulation_time)
 
 
 if __name__ == '__main__':
